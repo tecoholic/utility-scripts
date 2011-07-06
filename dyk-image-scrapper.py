dcb14cf23812eadf094561063a664840c08250f5
Arunmozhi <aruntheguy@gmail.com>
2011-07-06 11:48:49 +0530

    image scrapper works on 2011 archives

--- dyk-image-scrapper.py
+++ dyk-image-scrapper.py
@@ -1,3 +1,5 @@
+''' This file scrapes the images from the DYK archives and associates the images
+with the respective hooks '''
 import os
 import urllib
 import codecs
@@ -5,51 +7,52 @@ import codecs
 import BeautifulSoup
 
 directory = "hookhtmls"
-
-def extract_data(div, ul):
-    ''' This Function extracts the data from the div and the ul '''
-
-    imagefile = codecs.open("image.csv", encoding='utf-8', mode='w+')
-    
-    lis = ul.findAll('li',
-                     attrs={ 'style': '-moz-float-edge: content-box'} )
-    for index, li in enumerate(lis):
-        try:
-            link = li.b.a["href"]
-        except TypeError:
-            link = li.find("a")["href"]
-        link = link.replace("/wiki/","")
-        title = urllib.unquote(unicode(link).encode('ascii')).decode('utf-8')
-        title = " ".join(title.split('_'))
-        #print title
         
-        if index == 0:
-            image = div.find('img')
-            image_data = { 'alt' : image["alt"],
-                           'src' : image["src"] }
-    
 
 def main():
     ''' test main '''
+    sno = 1
     files = os.listdir(directory)
-    csvfile = codecs.open("data.csv", encoding='utf-8', mode='w+')
-    csvfile.write("id,link,text\n")
+    imagefile = codecs.open("images.csv", encoding='utf-8', mode='w+')
+    imagefile.write("sno,title,imgtitle,src,alt\n")
     for fil in files:
-        print fil
-        #csvfile = codecs.open(str(fil).replace("html","csv"), encoding='utf-8', mode='w+')
+        print fil        
         html = open(os.path.join(directory,fil), "r")
         soup = BeautifulSoup.BeautifulSoup(html.read())
         h3 = soup.find('h3')
-        firstDiv = h3.findNext('div',
+        div = h3.findNext('div',
+                          attrs={'style':'float:right;margin-left:0.5em;'})
+        #print div
+        while div != None:    
+            li = div.findNext("li",
+                              attrs={'style':'-moz-float-edge: content-box'})
+            try:
+                link = li.b.a['href']
+            except TypeError:
+                link = li.find("a")["href"]
+            link = link.replace("/wiki/","")
+            title = urllib.unquote(unicode(link).encode('ascii')).decode('utf-8')
+            title = " ".join(title.split('_')).replace(",",";")
+            print title
+            try:
+                imgAnchor = div.find("a", attrs={'class':'image'})
+                imgtitle = unicode(imgAnchor["title"]).replace(",",";")
+                img = imgAnchor.find("img")
+                src = unicode(img['src']).replace(",",";")
+                alt = unicode(img['alt']).replace(",",";")
+                
+                imagefile.write(str(sno)+","+title+","+imgtitle+","+src+","+alt+"\n")
+                sno += 1
+            except:
+                print "Image Unavailable"
+            div = div.findNext('div',
                                attrs={'style':'float:right;margin-left:0.5em;'})
-        #print unicode(firstDiv.p.a.img["alt"])
-        while firstDiv != None:
-            ul = firstDiv.findNext("ul")
-            extract_data(firstDiv, ul)
-            firstDiv = ul.findNext('div',
-                                   attrs={'style':'float:right;margin-left:0.5em;'})
-            break
-        break
+            #print "nextdiv",div.a["title"]
+    imagefile.close()
+                
+                
 
+
+        
 if __name__ == "__main__":
     main()
