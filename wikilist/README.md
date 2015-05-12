86f2d79319ea725fd160c239847da4f7602a8a09
tecoholic <arun@arunmozhi.in>
2015-05-12 18:54:04 +0530

    added wikilist

---
+++ wikilist/README.md
@@ -0,0 +1,3 @@
+# wikilist
+
+Download the list of wikipedia projects from the WMF Labs [wikistats](http://wikistats.wmflabs.org/) and produce a json file for use as a dropdownlist,
---
+++ wikilist/wikilist.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+"""
+http://wikistats.wmflabs.org/api.php
+
+Wikistats API
+
+current actions:
+
+    -  action=dump
+       -- dump csv, ssv or xml data of all tables
+
+       option 1: format (csv|ssv|xml)
+       option 2: table (wikipedias|wiktionaries|...)
+
+       example: api.php?action=dump&table=wikipedias&format=csv
+       example: api.php?action=dump&table=wikiquotes&format=ssv
+       example: api.php?action=dump&table=neoseeker&format=xml
+"""
+
+import urllib2
+import sys
+import json
+import codecs
+
+def prepare_json(project):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'WikiList Script')]
+    url = 'http://wikistats.wmflabs.org/api.php?action=dump&table={0}&format=csv'.format(project)
+    print 'Opening ',url
+    p = opener.open(url)
+    with codecs.open('list.csv', mode='w', encoding='utf-8') as f:
+        f.write(p.read().decode('utf-8'))
+    # build the required data
+    data = []
+    with codecs.open('list.csv', mode='r', encoding='utf-8') as csvfile:
+        header = csvfile.readline()
+        for line in csvfile.readlines():
+            row = line.split(u',')
+            data.append(dict(lang=row[1], prefix=row[2], loclang=row[10]))
+    with codecs.open(project+'.json', mode='w', encoding='utf-8') as jsonfile:
+        jsonfile.write(json.dumps(data))
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: python wikilist <project>\n\nSee list at http://wikistats.wmflabs.org/'
+    prepare_json(sys.argv[1])
